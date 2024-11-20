a81b8a6ee3a7cb92d3a8cff0f89b42c7cd466f33
giorgigiorga2009 <powerpointexpertgiorgi@gmail.com>
2024-11-20 19:02:29 +0400

    second

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,11 +1,11 @@
 import Counter from "@/app/_components/Counter";
+import CabinCard from "@/app/_components/CabinCard";
 
 export const metadata = {
   title: "Cabins",
 };
 
 export default function Page() {
-  // CHANGE
   const cabins = [];
 
   return (
@@ -17,7 +17,7 @@ export default function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature's beauty in your own little home
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little home
         away from home. The perfect spot for a peaceful, calm vacation. Welcome
         to paradise.
       </p>
